b7799b01f27c5be7d7899bd75b5a5521154a8167
yuyuaqwq <1454832774@qq.com>
2023-09-28 14:12:41 +0800

    完善测试用例

--- test.cpp
+++ test.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <fstream>
 
+#include <Geek/File/file.hpp>
+
 #define private public
 #include <pdbuilder/pdbuilder.hpp>
 #include <pdbuilder/downloader.hpp>
@@ -39,40 +41,16 @@ symbolic_access::Member Deserialize<symbolic_access::Member>(std::istream& is) {
 
 int main()
 {
-   /*
-    uint32_t* buf = new uint32_t[1024];
-    RingQueue<uint32_t> queue(buf, 1024);
-    clock_t start, end;
-    start = clock();
 
-
-    std::thread enqueue([&]() {
-        for (int i = 0; i < 10000000; i++) {
-            queue.Enqueue(100);
-        }
-        });
-
-    std::thread dequeue([&]() {
-        uint32_t a;
-        for (int i = 0; i < 10000000; i++) {
-            queue.Dequeue(&a);
-        }
-        });
-
-    enqueue.join();
-    dequeue.join();
-
-    end = clock();
-
-    std::cout << "F1运行时间" << (double)(end - start) << "ms" << std::endl;*/
-
-    pdbuilder::Downloader downloader;
-    downloader.DownloadPdb(downloader.GetPdbInfoFromImageBuf((uint8_t*)GetModuleHandleA("ntdll.dll")));
+    if (!Geek::File::FileExists(L"ntdll.dll")) {
+        pdbuilder::Downloader downloader;
+        downloader.DownloadPdb(downloader.GetPdbInfoFromImageBuf((uint8_t*)GetModuleHandleA("ntdll.dll")));
+    }
 
     std::fstream fs;
-    fs.open("test.txt", std::ios_base::in | std::ios_base::out | std::ios_base::trunc | std::ios_base::binary);
+    fs.open("ntdll.bin", std::ios_base::in | std::ios_base::out | std::ios_base::trunc | std::ios_base::binary);
 
-    pdbuilder::Pdber pdber{ pdbuilder::FileStream(LR"(C:\lib\SymbolicAccess-master\SymbolicAccessTest\TestData\x86_64\ntdll.pdb)") };
+    pdbuilder::Pdber pdber{ pdbuilder::FileStream(LR"(ntdll.pdb)") };
     sezz::Serialize(fs, pdber);
 
     fs.seekg(0);
@@ -84,14 +62,3 @@ int main()
 
     std::cout << "Hello World!\n";
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
